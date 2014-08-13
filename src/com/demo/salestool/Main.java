be0f935ffa6d1a8bbad3e2f40e8bee4ee8a04812
adamcgithub <adamcgithub@gmail.com>
2014-08-13 22:58:44 +0100

    Initial commit of app

---
+++ src/com/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+package com.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hello happy sales people");
+		System.out.println("This App shows sales data");
+	}
+
+}
