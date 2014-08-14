eb159c322734277a1397a6a1fe78fcfe4efdb027
adamcgithub <adamcgithub@gmail.com>
2014-08-14 21:30:55 +0100

    Added simple output message

--- src/com/demo/salestool/Main.java
+++ src/com/demo/salestool/Main.java
@@ -13,6 +13,8 @@ public class Main {
 	private static void displayGreeting(){
 		System.out.println("Hello happy sales people");
 		System.out.println("This App shows sales data");
+		
+		System.out.println("Test 1");
 	}
 
 }
